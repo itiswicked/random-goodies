86621fcd51dd87b9972b747416e9936e7247348e
Nathan Bourke <nathanrbourke@gmail.com>
2015-02-26 17:04:34 -0500

    Add notes to substring.rb

--- substrings.rb
+++ substrings.rb
@@ -7,10 +7,14 @@ string_2 = "Howdy partner, sit down! How's it going?"
 
 def substring(string, dictionary)
 	results = {}
+	#split string on non-word characters and iterate
 	string.downcase.split(/\W+/).each do |substring|
+		#iterate dictionary items over each input substring
 		dictionary.each do |word|
+			#convert each dictionary word to regexp
 			expression = Regexp.union(word)
 			if expression.match(substring)
+				#add key to results or modify existing keys
 				if results.has_key?(word)
 					results[word] += 1
 				else
@@ -22,4 +26,5 @@ def substring(string, dictionary)
 	puts results
 end
 
-substring(string_2,dictionary)
+substring(string_2,dictionary
+	# => {"how"=>2, "howdy"=>1, "part"=>1, "partner"=>1, "it"=>2, "i"=>3, "sit"=>1, "down"=>1, "own"=>1, "go"=>1, "going"=>1}
