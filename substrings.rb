2b76920de0a3a2cd4b0282f19524d1ab880bc868
Nathan <itiswicked@users.noreply.github.com>
2015-02-26 17:19:23 -0500

    "Fix typo"

--- substrings.rb
+++ substrings.rb
@@ -26,5 +26,5 @@ def substring(string, dictionary)
 	puts results
 end
 
-substring(string_2,dictionary
+substring(string_2,dictionary)
 	# => {"how"=>2, "howdy"=>1, "part"=>1, "partner"=>1, "it"=>2, "i"=>3, "sit"=>1, "down"=>1, "own"=>1, "go"=>1, "going"=>1}
